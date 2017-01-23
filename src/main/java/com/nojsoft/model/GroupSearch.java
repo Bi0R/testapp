cc75d9029a129b9b50b0c68a8a56948c97f0fc04
Bi0R <bior87@gmail.com>
2017-01-23 12:34:27 -0600

    Elimination Entity Annotation GroupSearch

--- src/main/java/com/nojsoft/model/GroupSearch.java
+++ src/main/java/com/nojsoft/model/GroupSearch.java
@@ -6,7 +6,6 @@ import javax.persistence.Entity;
  * Created by jorge on 23/01/17.
  */
 
-@Entity
 public class GroupSearch implements BaseModel {
     private String filter;
     private String value;
