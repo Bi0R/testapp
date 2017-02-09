7e0ecaf82f4841ec9ca78282eab999fa7fd28a49
Bi0R <bior87@gmail.com>
2017-02-09 16:34:03 -0600

    Fixed typo in Native Query

--- src/main/java/com/nojsoft/constants/DataBaseConstants.java
+++ src/main/java/com/nojsoft/constants/DataBaseConstants.java
@@ -13,7 +13,7 @@ public class DataBaseConstants {
     public static String USER_TOKEN_FIELD = "token";
 
     public static String GROUP_PARTICIPANT_QUERY = "SELECT g.* from groups g " +
-            "JOIN group_participants gp ON g.id = gp.group_id WHERE gp.status = 1 AND gp.user_id =:userId;";
+            "JOIN group_participants gp ON g.id = gp.group_id WHERE gp.status = 1 AND gp.user_id =:userId";
 
     public static int USER_REQUESTER = 0;
     public static int USER_ACCEPTED = 1;
