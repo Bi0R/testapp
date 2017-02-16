918a55d8e5ef3fdf19f53b03b6a466af3a093de3
Jorge Abreu <bior87@gmail.com>
2017-02-16 13:24:49 -0600

    Group search status (#10)
    * Group Search including status
    
    * Group Search debug
    
    * Group Search debug2
    
    * Group Search debug3
    
    * Fix typo in Native Query
    
    * Changed Query to include groups that are already in group_participants but don't include the user searching
    
    * Changed Query
    
    * reducing line lenght

--- src/main/java/com/nojsoft/dao/GroupDao.java
+++ src/main/java/com/nojsoft/dao/GroupDao.java
@@ -4,10 +4,12 @@ import com.nojsoft.constants.DataBaseConstants;
 import com.nojsoft.model.Group;
 import com.nojsoft.model.GroupParticipant;
 import org.hibernate.SQLQuery;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by alan on 1/14/17.
@@ -16,8 +18,21 @@ import java.util.List;
 @Repository("GroupDao")
 public class GroupDao extends GeneralDao {
 
+    @Autowired
+    UserDao userDao;
+
     public static String GROUP_PARTICIPANT_QUERY = "SELECT g.* from groups g " +
             "JOIN group_participants gp ON g.id = gp.group_id WHERE gp.status = 1 AND gp.user_id =:userId";
+
+    public static String GROUP_OWNER_SEARCH = "SELECT g.* FROM groups g" +
+            " JOIN group_participants gp ON g.id = gp.group_id AND gp.status NOT IN (0,1) AND gp.user_id = :userId" +
+            " WHERE  g.owner_id = :ownerId AND g.owner_id <> :userId" +
+            " UNION ALL" +
+            " SELECT g.* FROM groups g" +
+            " WHERE  g.owner_id = :ownerId AND g.owner_id <> :userId" +
+            " AND NOT EXISTS" +
+            " (SELECT gp.group_id FROM group_participants gp" +
+            " WHERE gp.user_id = :userId AND gp.group_id=g.id);";
 
 
     public Group saveOrUpdate(Group group) {
@@ -30,6 +45,14 @@ public class GroupDao extends GeneralDao {
 
     public List<Group> getGroupsByOwner(long ownerId) {
         return super.findByField(Group.class, DataBaseConstants.OWNER_ID_FIELD, ownerId);
+    }
+
+    public List<Group> getGroupsByOwnerStatus(long ownerId, long userId) {
+        SQLQuery query = super.getSession().createNativeQuery(GROUP_OWNER_SEARCH);
+        query.addEntity(Group.class);
+        query.setParameter(DataBaseConstants.OWNER_ID_FIELD, ownerId);
+        query.setParameter(DataBaseConstants.USER_ID_FIELD, userId);
+        return query.list();
     }
 
     public void requestAccess(GroupParticipant groupParticipant) {
@@ -52,5 +75,16 @@ public class GroupDao extends GeneralDao {
         query.addEntity(Group.class);
         query.setParameter(DataBaseConstants.USER_ID_FIELD, participantId);
         return query.list();
+    }
+
+    public Group getGroup(Group group) {
+        return super.findById(Group.class, group.getId());
+    }
+
+    public Group getFullGroup(Group group) {
+        group = getGroup(group);
+        group.setRequesters(userDao.getRequestersGroup(group));
+        group.setParticipants(userDao.getParticipantsGroup(group));
+        return group;
     }
 }
